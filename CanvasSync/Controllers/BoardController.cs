927a38506739f63764f3167a779cd1ce243aca19
Samuel Batey <sbatey01@gmail.com>
2026-08-14 16:09:20 +1000

    Created Board controller and moved the whiteboard from Index to its own view

---
+++ CanvasSync/Controllers/BoardController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CanvasSync.Controllers {
+    public class BoardController : Controller {
+        public IActionResult Board() {
+            return View();
+        }
+    }
+}
