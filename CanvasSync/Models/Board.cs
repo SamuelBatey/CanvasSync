4ddbe0cfa3956300a4f34eff8deb8489ebe90055
Samuel Batey <sbatey01@gmail.com>
2026-08-14 16:37:15 +1000

    Created Board model

---
+++ CanvasSync/Models/Board.cs
@@ -0,0 +1,8 @@
+﻿namespace CanvasSync.Models {
+    public class Board {
+        public string ID { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastModified { get; set; }
+        public string CanvasDataURL { get; set; }
+    }
+}
