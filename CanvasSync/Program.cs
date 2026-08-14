275e83294173a761f9653e9a37f9799148724501
Samuel Batey <sbatey01@gmail.com>
2026-08-14 17:01:39 +1000

    Register BoardContext and add db connection string

--- CanvasSync/Program.cs
+++ CanvasSync/Program.cs
@@ -1,7 +1,12 @@
+using CanvasSync.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BoardContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BoardContext")));
 
 var app = builder.Build();
 
@@ -10,6 +15,13 @@ if (!app.Environment.IsDevelopment()) {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<BoardContext>();
+    context.Database.EnsureCreated();
 }
 
 app.UseHttpsRedirection();
