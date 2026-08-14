092bc9400e9d850943a9c654951569f5341509b1
Samuel Batey <sbatey01@gmail.com>
2026-08-14 17:01:01 +1000

    Created BoardContext

---
+++ CanvasSync/Data/BoardContext.cs
@@ -0,0 +1,16 @@
+﻿using CanvasSync.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CanvasSync.Data {
+    public class BoardContext : DbContext {
+        public BoardContext(DbContextOptions<BoardContext> options) : base(options) {
+
+        }
+
+        public DbSet<Board> Boards { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Board>().ToTable("Board");
+        }
+    }
+}
