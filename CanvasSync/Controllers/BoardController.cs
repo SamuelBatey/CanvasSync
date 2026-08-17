1e2610e20e9e69980cd40925aa5edb1083deed21
Samuel Batey <sbatey01@gmail.com>
2026-08-17 12:05:21 +1000

    Can now have multiple boards that are assigned codes and signalr now groups clients by board

--- CanvasSync/Controllers/BoardController.cs
+++ CanvasSync/Controllers/BoardController.cs
@@ -1,9 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CanvasSync.Data;
+using CanvasSync.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CanvasSync.Controllers {
     public class BoardController : Controller {
-        public IActionResult Board() {
-            return View();
+        private readonly BoardContext _context;
+
+        public BoardController(BoardContext context) {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetBoard(string ID) {
+            return RedirectToAction("Board", new { ID = ID });
+        }
+
+        [Route("Board/Board/{id?}")]
+        public async Task<IActionResult> Board(string? id) {
+            if(id == null) {
+                return NotFound();
+            }
+            var board = await _context.Boards.FirstOrDefaultAsync(m => m.ID == id);
+            if(board == null) {
+                return NotFound();
+            }
+            return View(board);
+        }
+
+        public async Task<IActionResult> Create() {
+            Board board = new Board();
+            board.ID = Guid.NewGuid().ToString().Substring(0,5);
+            board.DateCreated = DateTime.Now;
+            board.LastModified = DateTime.Now;
+            board.CanvasDataURL = "temp";
+
+            _context.Add(board);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Board", new { ID = board.ID });
         }
     }
 }
--- CanvasSync/Hubs/BoardHub.cs
+++ CanvasSync/Hubs/BoardHub.cs
@@ -2,8 +2,13 @@
 
 namespace CanvasSync.Hubs {
     public class BoardHub : Hub {
-        public async Task SendBoard(int startX, int startY, int endX, int endY, int thickness, bool eraser) {
-            await Clients.All.SendAsync("ReceiveBoard", startX, startY, endX, endY, thickness, eraser);
+
+        public async Task SendBoard(int startX, int startY, int endX, int endY, int thickness, bool eraser, string group) {
+            await Clients.OthersInGroup(group).SendAsync("ReceiveBoard", startX, startY, endX, endY, thickness, eraser);
+        }
+
+        public async Task AddToGroup(string boardID) {
+            await Groups.AddToGroupAsync(Context.ConnectionId, boardID);
         }
     }
 }
