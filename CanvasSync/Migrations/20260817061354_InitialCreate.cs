36b58d132e13adf251e2def5d6681a6bd303bf7d
Samuel Batey <sbatey01@gmail.com>
2026-08-17 17:17:41 +1000

    Added db migrations

---
+++ CanvasSync/Migrations/20260817061354_InitialCreate.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CanvasSync.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Board",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Board", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Line",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BoardID = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Line", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Line_Board_BoardID",
+                        column: x => x.BoardID,
+                        principalTable: "Board",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Stroke",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartX = table.Column<int>(type: "int", nullable: false),
+                    StartY = table.Column<int>(type: "int", nullable: false),
+                    EndX = table.Column<int>(type: "int", nullable: false),
+                    EndY = table.Column<int>(type: "int", nullable: false),
+                    Thickness = table.Column<int>(type: "int", nullable: false),
+                    IsEraser = table.Column<bool>(type: "bit", nullable: false),
+                    LineID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stroke", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Stroke_Line_LineID",
+                        column: x => x.LineID,
+                        principalTable: "Line",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Line_BoardID",
+                table: "Line",
+                column: "BoardID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stroke_LineID",
+                table: "Stroke",
+                column: "LineID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stroke");
+
+            migrationBuilder.DropTable(
+                name: "Line");
+
+            migrationBuilder.DropTable(
+                name: "Board");
+        }
+    }
+}
