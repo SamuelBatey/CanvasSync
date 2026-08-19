1533136012bce70de71b60ef1063ed8354630c58
Samuel Batey <sbatey01@gmail.com>
2026-08-19 16:00:51 +1000

    Added initialisation of Line.Strokes to make sure no null reference errors happen

--- CanvasSync/Models/Line.cs
+++ CanvasSync/Models/Line.cs
@@ -4,6 +4,6 @@ namespace CanvasSync.Models {
     public class Line {
         public int ID { get; set; }
         public string BoardID { get; set; }
-        public ICollection<Stroke> Strokes { get; set; }
+        public ICollection<Stroke> Strokes { get; set; } = new List<Stroke>();
     }
 }
