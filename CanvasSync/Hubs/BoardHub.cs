e8c57b0f7e233d59b1127bbab27c91c72ac29368
Samuel Batey <sbatey01@gmail.com>
2026-08-21 13:35:47 +1000

    Added comments to board hub

--- CanvasSync/Hubs/BoardHub.cs
+++ CanvasSync/Hubs/BoardHub.cs
@@ -3,10 +3,12 @@
 namespace CanvasSync.Hubs {
     public class BoardHub : Hub {
 
+        // Sends the stroke to all other clients in the board group
         public async Task SendStroke(int startX, int startY, int endX, int endY, int thickness, bool eraser, string group) {
             await Clients.OthersInGroup(group).SendAsync("ReceiveStroke", startX, startY, endX, endY, thickness, eraser);
         }
 
+        // Adds the client to the given board group
         public async Task AddToGroup(string boardID) {
             await Groups.AddToGroupAsync(Context.ConnectionId, boardID);
         }
