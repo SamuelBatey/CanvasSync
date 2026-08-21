76e18cf71fe3c0bcaa9477649973bf01be186364
Samuel Batey <sbatey01@gmail.com>
2026-08-21 13:28:33 +1000

    Added comments and cleaned up board controller a bit

--- CanvasSync/Controllers/BoardController.cs
+++ CanvasSync/Controllers/BoardController.cs
@@ -12,29 +12,35 @@ namespace CanvasSync.Controllers {
             _context = context;
         }
 
+        // Redirects to the Board view with the specified ID
         [Route("Board/GetBoard")]
         [HttpPost]
         public async Task<IActionResult> GetBoard(string ID) {
+            // TODO: Check if the given ID corresponds to an existing board in the database
             return RedirectToAction("Board", new { ID = ID });
         }
 
+        // Saves a line in the database
         [Route("Board/SaveLine")]
         [HttpPost]
         public async Task<IActionResult> SaveLine([FromBody] Line line) {
-            if(string.IsNullOrEmpty(line.BoardID) || line == null) {
-                return BadRequest();
-            }
+            // Check data isn't null
+            if(string.IsNullOrEmpty(line.BoardID) || line == null) return BadRequest();
+
+            // Try to get the board from the database
             var boardToUpdate = await _context.Boards.Include(b => b.Lines).FirstOrDefaultAsync(b => b.ID == line.BoardID);
 
-            if(boardToUpdate == null) {
-                return NotFound();
-            }
+            // Make sure a board was found
+            if (boardToUpdate == null) return NotFound();
 
+            // Initialise the list that stores the strokes that make up the line, just to be 100% sure we don't get a null reference error
             line.Strokes ??= new List<Stroke>();
 
+            // Add the line to the board and update the last modified attribute
             boardToUpdate.Lines.Add(line);
             boardToUpdate.LastModified = DateTime.Now;
 
+            // Save changes to database
             _context.Update(boardToUpdate);
             await _context.SaveChangesAsync();
 
@@ -43,26 +49,33 @@ namespace CanvasSync.Controllers {
 
         [Route("Board/{id?}")]
         public async Task<IActionResult> Board(string? id) {
-            if(id == null) {
-                return NotFound();
-            }
+            // Check for a null id, just in case the value wasn't bound properly
+            if(id == null) return NotFound();
+
+            // Get the board from the database
             var board = await _context.Boards.Include(b => b.Lines).ThenInclude(l => l.Strokes).FirstOrDefaultAsync(m => m.ID == id);
-            if(board == null) {
-                return NotFound();
-            }
+            
+            // Make sure a board was found
+            if(board == null) return NotFound();
+
             return View(board);
         }
 
+        // Create new board
         [Route("Board/Create")]
         public async Task<IActionResult> Create() {
+            // Create board and set initial values
             Board board = new Board();
             board.ID = Guid.NewGuid().ToString().Substring(0,5);
             board.DateCreated = DateTime.Now;
             board.LastModified = DateTime.Now;
             board.Lines = new List<Line>();
 
+            // Add board and save it to the database
             _context.Add(board);
             await _context.SaveChangesAsync();
+
+            // Redirect to the newly created board
             return RedirectToAction("Board", new { ID = board.ID });
         }
     }
